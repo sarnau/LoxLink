c105d42f9d078136bd681a9b0b0b0bee5b1619de
Markus Fritze <github@sarnau.com>
2019-02-25 17:43:36 +0100

    Format

--- miniblinky/application_code/main.c
+++ miniblinky/application_code/main.c
@@ -10,87 +10,13 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-/**
-  * @brief  Sets System clock frequency to 72MHz and configure HCLK, PCLK2 
-  *         and PCLK1 prescalers. 
-  * @note   This function should be used only after reset.
-  * @param  None
-  * @retval None
-  */
-static void SetSysClockTo72(void)
-{
-  __IO uint32_t StartUpCounter = 0, HSEStatus = 0;
-  
-  /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration ---------------------------*/    
-  /* Enable HSE */    
-  RCC->CR |= ((uint32_t)RCC_CR_HSEON);
- 
-  /* Wait till HSE is ready and if Time out is reached exit */
-  do
-  {
-  } while(((RCC->CR & RCC_CR_HSERDY) == 0) && (StartUpCounter++ != HSE_STARTUP_TIMEOUT));
-  if ((RCC->CR & RCC_CR_HSERDY) != RESET)
-  {
-    /* Enable Prefetch Buffer */
-    FLASH->ACR |= FLASH_ACR_PRFTBE;
-
-    /* Flash 2 wait state */
-    FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
-    FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;    
-
-    /* HCLK = SYSCLK */
-    RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
-      
-    /* PCLK2 = HCLK */
-    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
-    
-    /* PCLK1 = HCLK */
-    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
-
-    /*  PLL configuration: PLLCLK = HSE * 9 = 72 MHz */
-    RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE |
-                                        RCC_CFGR_PLLMULL));
-    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC | RCC_CFGR_PLLMULL9);
-
-    /* Enable PLL */
-    RCC->CR |= RCC_CR_PLLON;
-
-    /* Wait till PLL is ready */
-    while((RCC->CR & RCC_CR_PLLRDY) == 0)
-    {
-    }
-    
-    /* Select PLL as system clock source */
-    RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-    RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;    
-
-    /* Wait till PLL is used as system clock source */
-    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL)
-    {
-    }
-  }
-}
-
-void SysTick_Handler(void) {
-  HAL_IncTick();
-  HAL_SYSTICK_IRQHandler();
-}
+static void SetSysClockTo72(void);
+static void prvInitializeHeap(void);
 
 void printCPUInfo() {
   uint32_t uid[3];
   HAL_GetUID(uid);
   printf("Clock:%dMHz/%dMHz FLASH:%dkb, Unique device ID:%x.%x.%x\n", SystemCoreClock / 1000000, HAL_RCC_GetSysClockFreq() / 1000000, LL_GetFlashSize(), uid[0], uid[1], uid[2]);
-}
-
-static void prvInitializeHeap(void) {
-  static uint8_t ucHeap1[configTOTAL_HEAP_SIZE];
-
-  HeapRegion_t xHeapRegions[] =
-      {
-          {(unsigned char *)ucHeap1, sizeof(ucHeap1)},
-          {NULL, 0}};
-
-  vPortDefineHeapRegions(xHeapRegions);
 }
 
 void vTaskCode(void *pvParameters) {
@@ -105,7 +31,7 @@ void vTaskCode(void *pvParameters) {
   HAL_GPIO_Init(GPIOB, &GPIO_Init);
 
   const TickType_t xDelay = 250 / portTICK_PERIOD_MS;
-  for (;;) {
+  while(1) {
     HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_13);
     vTaskDelay(xDelay);
     HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
@@ -127,4 +53,79 @@ void main(void) {
   configASSERT(xHandle);
 
   vTaskStartScheduler();
+}
+
+
+
+void SysTick_Handler(void) {
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+}
+
+
+/**
+  * @brief  Sets System clock frequency to 72MHz and configure HCLK, PCLK2 
+  *         and PCLK1 prescalers. 
+  * @note   This function should be used only after reset.
+  * @param  None
+  * @retval None
+  */
+static void SetSysClockTo72(void) {
+  __IO uint32_t StartUpCounter = 0, HSEStatus = 0;
+
+  /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration ---------------------------*/
+  /* Enable HSE */
+  RCC->CR |= ((uint32_t)RCC_CR_HSEON);
+
+  /* Wait till HSE is ready and if Time out is reached exit */
+  do {
+  } while (((RCC->CR & RCC_CR_HSERDY) == 0) && (StartUpCounter++ != HSE_STARTUP_TIMEOUT));
+  if ((RCC->CR & RCC_CR_HSERDY) != RESET) {
+    /* Enable Prefetch Buffer */
+    FLASH->ACR |= FLASH_ACR_PRFTBE;
+
+    /* Flash 2 wait state */
+    FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
+    FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+
+    /* HCLK = SYSCLK */
+    RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
+
+    /* PCLK2 = HCLK */
+    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
+
+    /* PCLK1 = HCLK */
+    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+
+    /*  PLL configuration: PLLCLK = HSE * 9 = 72 MHz */
+    RCC->CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE |
+                                         RCC_CFGR_PLLMULL));
+    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC | RCC_CFGR_PLLMULL9);
+
+    /* Enable PLL */
+    RCC->CR |= RCC_CR_PLLON;
+
+    /* Wait till PLL is ready */
+    while ((RCC->CR & RCC_CR_PLLRDY) == 0) {
+    }
+
+    /* Select PLL as system clock source */
+    RCC->CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_SW));
+    RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+
+    /* Wait till PLL is used as system clock source */
+    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL) {
+    }
+  }
+}
+
+static void prvInitializeHeap(void) {
+  static uint8_t ucHeap1[configTOTAL_HEAP_SIZE];
+
+  HeapRegion_t xHeapRegions[] =
+    {
+      {(unsigned char *)ucHeap1, sizeof(ucHeap1)},
+      {NULL, 0}};
+
+  vPortDefineHeapRegions(xHeapRegions);
 }
--- miniblinky/application_code/rtos_code.c
+++ miniblinky/application_code/rtos_code.c
@@ -4,15 +4,14 @@
 
 #include <stdio.h>
 
-void xPortSysTickHandler( void );
+void xPortSysTickHandler(void);
 
 /**
   * @brief  SYSTICK callback.
   * @retval None
   */
-void HAL_SYSTICK_Callback(void)
-{
-    xPortSysTickHandler( );
+void HAL_SYSTICK_Callback(void) {
+  xPortSysTickHandler();
 }
 
 /*-----------------------------------------------------------*/
@@ -27,9 +26,8 @@ void HAL_SYSTICK_Callback(void)
  * configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h.
  *
  */
-void vApplicationMallocFailedHook()
-{
-    printf( ( "ERROR: Malloc failed to allocate memory\r\n" ) );
+void vApplicationMallocFailedHook() {
+  printf(("ERROR: Malloc failed to allocate memory\r\n"));
 }
 
 /*-----------------------------------------------------------*/
@@ -37,27 +35,26 @@ void vApplicationMallocFailedHook()
 /* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
  * implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
  * used by the Idle task. */
-void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
-                                    StackType_t ** ppxIdleTaskStackBuffer,
-                                    uint32_t * pulIdleTaskStackSize )
-{
-/* If the buffers to be provided to the Idle task are declared inside this
- * function then they must be declared static - otherwise they will be allocated on
- * the stack and so not exists after this function exits. */
-    static StaticTask_t xIdleTaskTCB;
-    static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+  StackType_t **ppxIdleTaskStackBuffer,
+  uint32_t *pulIdleTaskStackSize) {
+  /* If the buffers to be provided to the Idle task are declared inside this
+   * function then they must be declared static - otherwise they will be allocated on
+   * the stack and so not exists after this function exits. */
+  static StaticTask_t xIdleTaskTCB;
+  static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
 
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle
-     * task's state will be stored. */
-    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+  /* Pass out a pointer to the StaticTask_t structure in which the Idle
+   * task's state will be stored. */
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
 
-    /* Pass out the array that will be used as the Idle task's stack. */
-    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+  /* Pass out the array that will be used as the Idle task's stack. */
+  *ppxIdleTaskStackBuffer = uxIdleTaskStack;
 
-    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-     * Note that, as the array is necessarily of type StackType_t,
-     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+   * Note that, as the array is necessarily of type StackType_t,
+   * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
 
 /*-----------------------------------------------------------*/
@@ -65,27 +62,26 @@ void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
 /* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
  * implementation of vApplicationGetTimerTaskMemory() to provide the memory that is
  * used by the RTOS daemon/time task. */
-void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
-                                     StackType_t ** ppxTimerTaskStackBuffer,
-                                     uint32_t * pulTimerTaskStackSize )
-{
-/* If the buffers to be provided to the Timer task are declared inside this
- * function then they must be declared static - otherwise they will be allocated on
- * the stack and so not exists after this function exits. */
-    static StaticTask_t xTimerTaskTCB;
-    static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+  StackType_t **ppxTimerTaskStackBuffer,
+  uint32_t *pulTimerTaskStackSize) {
+  /* If the buffers to be provided to the Timer task are declared inside this
+   * function then they must be declared static - otherwise they will be allocated on
+   * the stack and so not exists after this function exits. */
+  static StaticTask_t xTimerTaskTCB;
+  static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
 
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle
-     * task's state will be stored. */
-    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+  /* Pass out a pointer to the StaticTask_t structure in which the Idle
+   * task's state will be stored. */
+  *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
 
-    /* Pass out the array that will be used as the Timer task's stack. */
-    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+  /* Pass out the array that will be used as the Timer task's stack. */
+  *ppxTimerTaskStackBuffer = uxTimerTaskStack;
 
-    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-     * Note that, as the array is necessarily of type StackType_t,
-     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+  /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+   * Note that, as the array is necessarily of type StackType_t,
+   * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+  *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
 }
 
 /**
@@ -99,11 +95,11 @@ void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
  * has occurred.
  *
  */
-void vApplicationStackOverflowHook( TaskHandle_t xTask,
-                                    char * pcTaskName )
-{
-    portDISABLE_INTERRUPTS();
+void vApplicationStackOverflowHook(TaskHandle_t xTask,
+  char *pcTaskName) {
+  portDISABLE_INTERRUPTS();
 
-    /* Loop forever */
-    for( ; ; );
+  /* Loop forever */
+  for (;;)
+    ;
 }
