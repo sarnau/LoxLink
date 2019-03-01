7973cb4b8b676e7f29d559e7a66ace06ad5fa058
Markus Fritze <github@sarnau.com>
2019-03-01 22:04:58 +0100

    Cleanup

--- miniblinky/application_code/system.c
+++ miniblinky/application_code/system.c
@@ -45,7 +45,6 @@ void SystemClock_Config(void) {
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
     for (;;) {
     }
