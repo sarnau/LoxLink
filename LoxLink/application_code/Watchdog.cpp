f6c638e9c3f4e1b8482dfc43947871360882954b
Markus Fritze <github@sarnau.com>
2019-03-10 01:48:27 +0100

    IWDG update to 10s, fixed comments

--- LoxLink/application_code/Watchdog.cpp
+++ LoxLink/application_code/Watchdog.cpp
@@ -20,16 +20,16 @@ void vWatchdogTask(void *pvParameters) {
  ***/
 void Start_Watchdog(void) {
   /* 
-        Watchdog freq. is 32 kHz
+        Watchdog freq. is 40 kHz
         Prescaler: Min_Value = 4 and Max_Value = 256
         Reload: Min_Data = 0 and Max_Data = 0x0FFF
         TimeOut in seconds = (Reload * Prescaler) / Freq.
-        MinTimeOut = (4 * 1) / 32000 = 0.000125 seconds (125 microseconds)
-        MaxTimeOut = (256 * 4096) / 32000 = 32.768 seconds
+        MinTimeOut = (4 * 1) / 40000 = 0.0001 seconds (100 microseconds)
+        MaxTimeOut = (256 * 4096) / 40000 = 26.2 seconds
     */
   gIWDG.Instance = IWDG;
   gIWDG.Init.Prescaler = IWDG_PRESCALER_256;
-  gIWDG.Init.Reload = 1500; // about 10s
+  gIWDG.Init.Reload = 1562; // about 10s
   HAL_IWDG_Init(&gIWDG);
 
   static StackType_t sWatchdogTaskStack[configMINIMAL_STACK_SIZE];
