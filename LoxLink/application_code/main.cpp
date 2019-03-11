677262dab241b18232aa05bab65706258102058b
Markus Fritze <github@sarnau.com>
2019-03-11 10:18:43 +0100

    Clarify serial number code

--- LoxLink/application_code/main.cpp
+++ LoxLink/application_code/main.cpp
@@ -17,12 +17,15 @@ int main(void) {
   static eAliveReason_t sResetReason;
   sResetReason = MX_reset_reason();
 
+  // generate a base serial from the STM32 UID
+  // Warning: be aware that two relay extension need two different serial numbers!
   uint32_t uid[3];
   HAL_GetUID(uid);
+  uint32_t serial_base = (uid[0] ^ uid[1] ^ uid[2]) & 0xFFFFFF;
 
   static LoxCANDriver gLoxCANDriver(tLoxCANDriverType_LoxoneLink);
-  static LoxBusDIExtension gDIExtension(gLoxCANDriver, (uid[0] ^ uid[1] ^ uid[2]) & 0xFFFFFF, sResetReason);
-  static LoxLegacyRelayExtension gRelayExtension(gLoxCANDriver, (uid[0] ^ uid[1] ^ uid[2]) & 0xFFFFFF);
+  static LoxBusDIExtension gDIExtension(gLoxCANDriver, serial_base, sResetReason);
+  static LoxLegacyRelayExtension gRelayExtension(gLoxCANDriver, serial_base);
 
 #if DEBUG && 0
   MX_print_cpu_info();
