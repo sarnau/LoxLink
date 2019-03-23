6051e9bbca50ca2e17cba2e9c4e5ad76c8de4b36
Markus Fritze <github@sarnau.com>
2019-03-23 23:35:02 +0100

    Log RGB values in percent and decode Lumitech values

--- LoxLink/application_code/Loxone/Legacy/LoxLegacyDMXExtension.cpp
+++ LoxLink/application_code/Loxone/Legacy/LoxLegacyDMXExtension.cpp
@@ -76,54 +76,70 @@ void LoxLegacyDMXExtension::PacketToExtension(LoxCanMessage &message) {
   }
 }
 
-const char * const LoxLegacyDMXExtension::DMX_DeviceTypeString(uint8_t type) const {
-    switch(type) {
-    case 0: return "Standard";
-    case 1: return "Standard RGB";
-    case 2: return "Standard RGBW";
-    case 3: return "Standard Lumitech";
-    case 8: return "Smart";
-    case 9: return "Smart RGB";
-    case 10: return "Smart RGBW";
-    case 11: return "Smart Lumitech";
-    }
-    return "Unknown";
+const char *const LoxLegacyDMXExtension::DMX_DeviceTypeString(uint8_t type) const {
+  switch (type) {
+  case 0:
+    return "Standard";
+  case 1:
+    return "Standard RGB";
+  case 2:
+    return "Standard RGBW";
+  case 3:
+    return "Standard Lumitech";
+  case 8:
+    return "Smart";
+  case 9:
+    return "Smart RGB";
+  case 10:
+    return "Smart RGBW";
+  case 11:
+    return "Smart Lumitech";
+  }
+  return "Unknown";
 }
 
 void LoxLegacyDMXExtension::FragmentedPacketToExtension(LoxMsgLegacyFragmentedCommand_t fragCommand, const void *fragData, int size) {
   // WARNING The Lumitech case is very different compared to RGB or RGBW:
   // data[0] =   0  => Lumitech Dual White
-  // data[1] = Brightness in % (0…255 => 0…100%)
-  // data[2] = Color temperature in Kevin (0…255 => 2700K…6500K)
+  // data[1] = Brightness in % (0-255 => 0-100%)
+  // data[2] = Color temperature in Kevin (0-255 => 2700K-6500K)
   // data[3] = unused
-  
+
   // data[0] = 101  => Lumitech RGB
-  // data[1] = Red in % (0…255 => 0…100%)
-  // data[2] = Blue in % (0…255 => 0…100%)
-  // data[3] = Green in % (0…255 => 0…100%)
+  // data[1] = Red in % (0-255 => 0-100%)
+  // data[2] = Blue in % (0-255 => 0-100%)
+  // data[3] = Green in % (0-255 => 0-100%)
 
   // Gamma == is the perception correction in the Loxone Config. It means that the output value is squared before transmission.
   switch (fragCommand) {
   case FragCmd_DMX_actor: {
     const DMX_Actor *package = (const DMX_Actor *)fragData;
-    const char * const gammaStr = ((package->Slewrate & 0x80) == 0x80) ? "yes" : "no";
+    const char *const gammaStr = ((package->Slewrate & 0x80) == 0x80) ? "yes" : "no";
     printf("DMX actor: Type:%s, Slewrate: %d%%, Gamma:%s, Channel:%d, Data:%d %d %d %d\n", DMX_DeviceTypeString(package->Type), package->Slewrate & 0x7F, gammaStr, package->Channel, package->Data[0], package->Data[1], package->Data[2], package->Data[3]);
     break;
   }
   case FragCmd_DMX_dimming: {
     const DMX_Dimming *package = (const DMX_Dimming *)fragData;
-    const char * const gammaStr = ((package->actor.Slewrate & 0x80) == 0x80) ? "yes" : "no";
+    const char *const gammaStr = ((package->actor.Slewrate & 0x80) == 0x80) ? "yes" : "no";
     printf("DMX dimming: Type:%s, Slewrate: %d%%, Gamma:%s, Channel:%d, Data:%d %d %d %d, DeviceId:0x%08x\n", DMX_DeviceTypeString(package->actor.Type), package->actor.Slewrate & 0x7F, gammaStr, package->actor.Channel, package->actor.Data[0], package->actor.Data[1], package->actor.Data[2], package->actor.Data[3], package->DeviceId);
     break;
   }
   case FragCmd_DMX_composite_actor: {
     const DMX_Composite *package = (const DMX_Composite *)fragData;
-    const char * const gammaStr = ((package->actor.Slewrate & 0x80) == 0x80) ? "yes" : "no";
+    const char *const gammaStr = ((package->actor.Slewrate & 0x80) == 0x80) ? "yes" : "no";
     uint16_t timeInMs = package->Time * 100; // time in 100ms units
-    if(timeInMs & 0x4000) // time in seconds?
-        timeInMs *= 10;
-    const char * const percentStr = ((package->Time & 0x8000) == 0x8000) ? "/100%" : "";
-    printf("DMX composite: Type:%s, Slewrate: %d%%, Gamma:%s, Channel:%d, Data:%d %d %d %d, Time:%dms%s\n", DMX_DeviceTypeString(package->actor.Type), package->actor.Slewrate & 0x7F, gammaStr, package->actor.Channel, package->actor.Data[0], package->actor.Data[1], package->actor.Data[2], package->actor.Data[3], timeInMs, percentStr);
+    if (timeInMs & 0x4000)                   // time in seconds?
+      timeInMs *= 10;
+    const char *const percentStr = ((package->Time & 0x8000) == 0x8000) ? "/100%" : "";
+    if (package->actor.Type == 11) {       // Lumitech
+      if (package->actor.Data[0] == 101) { // RGB
+        printf("DMX composite: Type:%s, Slewrate: %d%%, Gamma:%s, Channel:%d, RGB:%.1f%% %.1f%% %.1f%%, Time:%dms%s\n", DMX_DeviceTypeString(package->actor.Type), package->actor.Slewrate & 0x7F, gammaStr, package->actor.Channel, package->actor.Data[1] * 100.0 / 255, package->actor.Data[2] * 100.0 / 255, package->actor.Data[3] * 100.0 / 255, timeInMs, percentStr);
+      } else {
+        printf("DMX composite: Type:%s, Slewrate: %d%%, Gamma:%s, Channel:%d, Dual White:%.1f%% %dK, Time:%dms%s\n", DMX_DeviceTypeString(package->actor.Type), package->actor.Slewrate & 0x7F, gammaStr, package->actor.Channel, package->actor.Data[1] * 100.0 / 255, (package->actor.Data[2] * (6500 - 2700)) / 255 + 2700, timeInMs, percentStr);
+      }
+    } else {
+      printf("DMX composite: Type:%s, Slewrate: %d%%, Gamma:%s, Channel:%d, Data:%.1f%% %.1f%% %.1f%% %.1f%%, Time:%dms%s\n", DMX_DeviceTypeString(package->actor.Type), package->actor.Slewrate & 0x7F, gammaStr, package->actor.Channel, package->actor.Data[0] * 100.0 / 255, package->actor.Data[1] * 100.0 / 255, package->actor.Data[2] * 100.0 / 255, package->actor.Data[3] * 100.0 / 255, timeInMs, percentStr);
+    }
     break;
   }
   case FragCmd_DMX_init_rdm_device: {
